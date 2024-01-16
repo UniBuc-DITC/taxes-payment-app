52bc0fbebc7912aaf94c5beb5bf5d6710d4e7f7c
MocicaRazvan <razvanmocica@gmail.com>
2024-01-16 06:55:21 +0200

    Added static rendering to wrapper signin page

--- src/app/[locale]/auth/signin/page.tsx
+++ src/app/[locale]/auth/signin/page.tsx
@@ -1,7 +1,13 @@
 import { useTranslations } from "next-intl";
 import SignInButton from "./SignInButton";
+import { unstable_setRequestLocale } from "next-intl/server";
 
-export default function SignInPage() {
+interface PageProps {
+  params: { locale: string };
+}
+
+export default function SignInPage({ params: { locale } }: PageProps) {
+  unstable_setRequestLocale(locale);
   const t = useTranslations("Auth.SignIn");
 
   return (
