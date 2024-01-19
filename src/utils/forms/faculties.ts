957ff9c69f6585e2c6047be6481c08f3ce031597
MocicaRazvan <razvanmocica@gmail.com>
2024-01-19 17:09:32 +0200

    Added faculties utils to create from faculties with tax faculty tax options

---
+++ src/utils/forms/faculties.ts
@@ -0,0 +1,41 @@
+/**
+ *
+ * @remarks The labels int this methods are just a placeholder for when we the full ui
+ */
+
+import {
+  FacultyOption,
+  FacultyTaxOption,
+  FacultyWithTax,
+} from "@/types/forms/faculties";
+
+export function createFacultyTaxOptions(
+  faculties: FacultyWithTax[],
+  locale: string,
+) {
+  const isEnglish = locale === "en";
+  return faculties.reduce(
+    (acc, { id, nameEn, nameRo, facultyTaxValues }) => {
+      acc.facultyOptions.push({
+        id,
+        label: isEnglish ? nameEn : nameRo,
+      });
+      acc.taxesOptions[id.toString()] = facultyTaxValues.map(
+        ({ id, value, remarksEn, remarksRo, studyCycle }) => ({
+          id,
+          value: Number(value),
+          label: `${value} ${isEnglish ? remarksEn || "" : remarksRo || ""}`,
+          studyCycle,
+        }),
+      );
+      return acc;
+    },
+    {
+      facultyOptions: [],
+      taxesOptions: {},
+    } as {
+      facultyOptions: FacultyOption[];
+      taxesOptions: Record<string, FacultyTaxOption[]>;
+    },
+  );
+}
