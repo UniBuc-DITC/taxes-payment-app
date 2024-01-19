58ee19d7f2e87e38a58d4063ec044148491cb7ad
MocicaRazvan <razvanmocica@gmail.com>
2024-01-19 17:19:50 +0200

    Small changes to the placeholder navbar

--- src/components/reusable/Navbar.tsx
+++ src/components/reusable/Navbar.tsx
@@ -4,11 +4,8 @@ import LocaleSwitcher from "./LocaleSwitcher";
 export default function Navbar() {
   return (
     <div className="bg-blue-400 ">
-      <div className="max-w-7xl flex w-full justify-start gap-5 items-center mx-auto">
+      <div className="max-w-7xl flex w-full justify-between gap-5 items-center mx-auto">
         <Link href="/">Home</Link>
-        <Link href="/admission">Admission</Link>
-        <Link href="/tuition">Tuition</Link>
-        <Link href="/dorms">Dorms</Link>
         <LocaleSwitcher />
       </div>
     </div>
