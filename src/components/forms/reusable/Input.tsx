51d76532c8c11bdae66f13e58162206fc791cb0b
MocicaRazvan <razvanmocica@gmail.com>
2024-02-11 12:27:17 +0200

    Refactored the input to accept the base input props also

--- src/components/forms/reusable/Input.tsx
+++ src/components/forms/reusable/Input.tsx
@@ -6,7 +6,10 @@ import {
   UseFormRegister,
 } from "react-hook-form";
 
-type InputProps<TFieldValues extends FieldValues> = {
+type InputProps<TFieldValues extends FieldValues> = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "required"
+> & {
   id: string;
   label: string;
   type?: "text" | "email" | "password" | "number";
@@ -28,6 +31,7 @@ export default function Input<TFieldValues extends FieldValues>({
   required,
   placeholder = "",
   errors,
+  ...props
 }: InputProps<TFieldValues>) {
   return (
     <div className="relative">
@@ -35,10 +39,11 @@ export default function Input<TFieldValues extends FieldValues>({
         {label}
       </label>
       <input
+        {...props}
         {...register(name, { required, ...registerOptions })}
         id={id}
         aria-label={name}
-        className={`mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2 outline-none focus:outline-blue-200 ${
+        className={`mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 p-2 outline-none focus:outline-blue-200 mb-1 ${
           errors[name] ? "outline outline-1 outline-red-400" : ""
         }`}
         placeholder={placeholder}
