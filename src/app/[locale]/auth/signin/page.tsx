9eb2506a0c929ff9171f2adedf182676f651c135
Gabriel Majeri <gabriel.majeri6@gmail.com>
2024-03-13 23:34:32 +0200

    Sample code for retrieving AAD user object ID

--- src/app/[locale]/auth/signin/page.tsx
+++ src/app/[locale]/auth/signin/page.tsx
@@ -1,8 +1,17 @@
+import { getServerSession } from "next-auth";
 import SignInButton from "./SignInButton";
+import { authOptions } from "@/next-auth-options";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const session = await getServerSession(authOptions);
+
   return (
     <div>
+      {session?.user && (
+        <p>
+          Azure AD user object ID: <code>{session.user.azureAdObjectId}</code>
+        </p>
+      )}
       <SignInButton />
     </div>
   );
